e15aa793d5f94aaf73377ffcc3d4f22e663bda6a
Hassen <hassen.saidi@gmail.com>
2024-10-18 21:42:43 -0700

    Update cJSON.rs

--- src/cJSON.rs
+++ src/cJSON.rs
@@ -2,3 +2,23 @@
 This file contains the main cJSON file.
 This file is the translation of the original cJSON C implementation in Rust.
 */
+
+
+// Project version
+const CJSON_VERSION_MAJOR: u32 = 1;
+const CJSON_VERSION_MINOR: u32 = 7;
+const CJSON_VERSION_PATCH: u32 = 15;
+
+// cJSON Types
+const cJSON_Invalid: u32 = 0;
+const cJSON_False: u32 = 1 << 0;
+const cJSON_True: u32 = 1 << 1;
+const cJSON_NULL: u32 = 1 << 2;
+const cJSON_Number: u32 = 1 << 3;
+const cJSON_String: u32 = 1 << 4;
+const cJSON_Array: u32 = 1 << 5;
+const cJSON_Object: u32 = 1 << 6;
+const cJSON_Raw: u32 = 1 << 7; // raw JSON
+
+const cJSON_IsReference: u32 = 256;
+const cJSON_StringIsConst: u32 = 512;
