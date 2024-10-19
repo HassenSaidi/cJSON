00b4048d120622b2ddcae16812fd38b98f4423ab
Hassen <hassen.saidi@gmail.com>
2024-10-19 16:50:45 -0700

    Update cJSON.rs

--- src/cJSON.rs
+++ src/cJSON.rs
@@ -22,3 +22,18 @@ const cJSON_Raw: u32 = 1 << 7; // raw JSON
 
 const cJSON_IsReference: u32 = 256;
 const cJSON_StringIsConst: u32 = 512;
+
+
+// CJSON structure
+// type is renamed to type_
+#[derive(Debug)]
+pub struct CJSON {
+    pub next: Option<Box<CJSON>>,
+    pub prev: Option<Box<CJSON>>, // Raw mutable pointer
+    pub child: Option<Box<CJSON>>,
+    pub type_: i32,
+    pub valuestring: Option<String>,
+    pub valueint: i32,
+    pub valuedouble: f64,
+    pub string: Option<String>,
+}
