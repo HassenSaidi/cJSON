366d7d0badb7d96e8fcfea5138b4a51a1bf5d77c
Hassen <hassen.saidi@gmail.com>
2024-10-22 21:13:11 -0700

    Update cJSON.rs

--- src/cJSON.rs
+++ src/cJSON.rs
@@ -15,3 +15,25 @@ const cJSON_Raw: u32 = 1 << 7; /* raw json */
 
 const cJSON_IsReference: u32 = 256;
 const cJSON_StringIsConst: u32 = 512;
+
+/* The cJSON structure: */
+struct cJSON {
+    /* next/prev allow you to walk array/object chains. Alternatively, use GetArraySize/GetArrayItem/GetObjectItem */
+    next: Option<Box<cJSON>>,
+    prev: Option<Box<cJSON>>,
+    /* An array or object item will have a child pointer pointing to a chain of the items in the array/object. */
+    child: Option<Box<cJSON>>,
+
+    /* The type of the item, as above. */
+    item_type: u32,
+
+    /* The item's string, if item_type == cJSON_String and item_type == cJSON_Raw */
+    valuestring: Option<String>,
+    /* writing to valueint is DEPRECATED, use cJSON_SetNumberValue instead */
+    valueint: i32,
+    /* The item's number, if item_type == cJSON_Number */
+    valuedouble: f64,
+
+    /* The item's name string, if this item is the child of, or is in the list of subitems of an object. */
+    string: Option<String>,
+}
